14b75d8cf314f6c77242f580e70c3ab3c4614d06
Maxime Robin <robin.maxime@netcourrier.com>
2016-03-31 15:57:50 +0200

    CSV File upload V1

--- src/AppBundle/Controller/BaseController.php
+++ src/AppBundle/Controller/BaseController.php
@@ -3,8 +3,14 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Company;
+use AppBundle\Entity\Formation;
+use AppBundle\Entity\FormationPeriod;
+use AppBundle\Entity\School;
+use Doctrine\ORM\EntityManager;
+use parseCSV;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use SimpleXMLElement;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Constraints\DateTime;
@@ -17,6 +23,14 @@ class BaseController extends Controller
      */
     public function indexAction()
     {
+        return $this->render(':default:index.html.twig');
+    }
+
+    /**
+     * @Route("/flux/xml")
+     */
+    public function fluxXmlAction()
+    {
         $fluxService = $this->get('get_object_from_flux');
 
         $begin = new \DateTime("2016-03-15");
@@ -28,7 +42,102 @@ class BaseController extends Controller
     }
 
     /**
-     * @Route("/company/seed")
+     * @Route("/flux/csv")
+     */
+    public function fluxCSVAction(Request $request)
+    {
+
+        $form = $this->createFormBuilder()
+            ->add('csv_file', 'file', array('label' => 'File to Submit'))
+            ->add('schools', EntityType::class, array(
+                'class' => 'AppBundle:School',
+                'choice_label' => 'name',
+            ))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Get file
+            $file = $form->get('csv_file');
+            $school = $form->get('schools')->getData();
+
+            // Your csv file here when you hit submit button
+            $csv = new parseCSV();
+            $csv->auto($file->getData());
+
+            $em = $this->getDoctrine()->getManager();
+
+            foreach($csv->data as $data) {
+                $formation = new Formation();
+                $formation->setTitle($data['titre']);
+                $formation->setDescription($data['description']);
+                $formation->setPerspective($data['perspective']);
+                $formation->setUrl($data['url']);
+                $formation->setMail($data['mail']);
+                $formation->setSchool($school);
+                $formation->setIsGeoloc(true);
+                $formation->setStatus(0);
+
+                $geo = file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?address='.urlencode($data['adresse']).'&sensor=false');
+                $geo = json_decode($geo, true);
+                if ($geo['status'] === 'OK') {
+                    $formation->setAddress($geo['results'][0]['formatted_address']);
+                    $formation->setLatitude($geo['results'][0]['geometry']['location']['lat']);
+                    $formation->setLongitude($geo['results'][0]['geometry']['location']['lng']);
+
+                    var_dump($geo['results'][0]['address_components']);
+
+                    foreach($geo['results'][0]['address_components'] as $component) {
+                        switch ($component['types'][0]) {
+                            case 'locality':
+                                $formation->setCity($component['long_name']);
+                                break;
+                            case 'postal_code':
+                                $formation->setZip($component['long_name']);
+                                break;
+                            case 'administrative_area_level_2':
+                                $department = $em->getRepository('AppBundle:Department')->findOneByName(strtoupper($component['long_name']));
+                                if($department) {
+                                    $formation->setDepartment($department);
+                                }
+                                break;
+                        }
+                    }
+                }
+
+
+                $em->persist($formation);
+                $em->flush();
+
+                $begin_date = explode(', ', $data['date_debut']);
+                $end_date = explode(', ', $data['date_fin']);
+
+                for($i = 0; $i < count($begin_date); $i++) {
+                    $begin = new \DateTime($begin_date[$i]);
+                    $end = new \DateTime($end_date[$i]);
+
+                    $period = new FormationPeriod();
+                    $period->setBeginDate($begin);
+                    $period->setEndDate($end);
+                    $period->setFormation($formation);
+
+                    $em->persist($period);
+                    $em->flush();
+                }
+
+                var_dump($formation);
+            }
+            die;
+        }
+
+        return $this->render(':default:csv.html.twig',
+            array('form' => $form->createView(),)
+        );
+    }
+
+    /**
+     * @Route("/seed/company")
      */
     public function seedCompanyAction()
     {
@@ -40,11 +149,27 @@ class BaseController extends Controller
         $user->setLastname('Robin');
         $user->setFirstname('Maxime');
         $user->setEnabled(true);
-        $user->setRoles(['ROLE_SCHOOL']);
+        $user->setRoles(['ROLE_COMPANY']);
         $user->setPhone('0606060606');
 
         $company = new Company();
         $company->setName('Sogeti');
+        $address = "27, bis rue du progrès, 93100 Montreuil";
+        $geo = file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?address='.urlencode($address).'&sensor=false');
+        $geo = json_decode($geo, true);
+        if ($geo['status'] === 'OK') {
+            $company->setAddress($geo['results'][0]['formatted_address']);
+            $company->setLatitude($geo['results'][0]['geometry']['location']['lat']);
+            $company->setLongitude($geo['results'][0]['geometry']['location']['lng']);
+
+            foreach($geo['results'][0]['address_components'] as $component) {
+                switch ($component['types'][0]) {
+                    case 'locality':
+                        $company->setCity($component['long_name']);
+                        break;
+                }
+            }
+        }
         $company->setFluxXml("http://robinmaxime.com/flux.php");
         //"http://jobs.contactrh.com/get/XAePWe9fIg0G?client=sogeti"
 
@@ -61,10 +186,49 @@ class BaseController extends Controller
     }
 
     /**
-     * @Route("/flux/xml")
+     * @Route("/seed/school")
      */
-    public function fluxXmlAction()
+    public function seedSchoolAction()
     {
 
+        $user = new User();
+        $user->setUsername('school@gmail.com');
+        $user->setEmail('school@gmail.com');
+        $user->setPassword('toto');
+        $user->setLastname('Robin');
+        $user->setFirstname('Maxime');
+        $user->setEnabled(true);
+        $user->setRoles(['ROLE_SCHOOL']);
+        $user->setPhone('0606060606');
+
+        $school = new School();
+        $school->setName('HETIC');
+        $school->getIsOnlyOnline(false);
+        $address = "27, bis rue du progrès, 93100 Montreuil";
+        $geo = file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?address='.urlencode($address).'&sensor=false');
+        $geo = json_decode($geo, true);
+        if ($geo['status'] === 'OK') {
+            $school->setAddress($geo['results'][0]['formatted_address']);
+            $school->setLatitude($geo['results'][0]['geometry']['location']['lat']);
+            $school->setLongitude($geo['results'][0]['geometry']['location']['lng']);
+
+            foreach($geo['results'][0]['address_components'] as $component) {
+                switch ($component['types'][0]) {
+                    case 'locality':
+                        $school->setCity($component['long_name']);
+                        break;
+                }
+            }
+        }
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($school);
+        $em->flush();
+
+        $user->setSchool($school);
+
+        $em->persist($user);
+        $em->flush();
+
+        return $this->render(':default:index.html.twig');
     }
 }
--- src/AppBundle/Service/GetObjectFromFlux.php
+++ src/AppBundle/Service/GetObjectFromFlux.php
@@ -23,6 +23,86 @@ class GetObjectFromFlux
 
         $companies = $this->company->findAll();
 
+        foreach ($companies as $company) {
+            $flux = $company->getFluxXml();
+
+            // get array of offer from flux xml
+            $offers = simplexml_load_file($flux);
+
+            // foreach all offer
+            foreach ($offers as $offer) {
+
+                // Create Datetime object of the offer's datecreation
+                $creationDate = new \DateTime($offer->DATECREATION);
+
+                $address = $offer->ADDRESSE;
+
+                // if Datecreation is between begin and end
+                if ($creationDate >= $begin && $creationDate <= $end) {
+                    $begin_date = ($offer->DATEDEBUT) ? new \DateTime($offer->DATEDEBUT) : null;
+                    $end_date = ($offer->DATEFIN) ? new \DateTime($offer->DATEFIN) : null;
+
+                    $job = new Job();
+
+                    $job->setTitle((string)$offer->INTITULE);
+                    $job->setDescription((string)$offer->PRESENTATION);
+                    $job->setPrerequisite((string)$offer->PROFILCANDIDAT);
+                    $job->setBeginDate($begin_date);
+                    $job->setEndDate($end_date);
+                    $job->setUrl((string)$offer->URL);
+                    $job->setMail((string)$offer->MAIL_ENVOI_CV);
+                    $job->setIsGeoloc(true);
+                    $job->setSalaryMin((int)$offer->REMUNERATION_MIN);
+                    $job->setSalaryMax((int)$offer->REMUNERATION_MAX);
+                    $job->setSalaryType((string)$offer->REMUNERATION_F);
+                    $job->setStatus(0);
+                    $job->setCompany($company);
+
+                    $flux_job_field = $this->flux_job_field->findOneOrCreate(['name' => (string)$offer->FONCTION]);
+                    $job->setFluxJobField($flux_job_field);
+
+                    $flux_job_type = $this->flux_job_type->findOneOrCreate(['name' => (string)$offer->TYPECONTRAT]);
+                    $job->setFluxJobType($flux_job_type);
+
+                    // Geolocation
+                    $geo = file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?address=' . urlencode($address) . '&sensor=false');
+                    $geo = json_decode($geo, true);
+                    if ($geo['status'] === 'OK') {
+                        $job->setAddress($geo['results'][0]['formatted_address']);
+                        $job->setLatitude($geo['results'][0]['geometry']['location']['lat']);
+                        $job->setLongitude($geo['results'][0]['geometry']['location']['lng']);
+
+                        foreach ($geo['results'][0]['address_components'] as $component) {
+                            switch ($component['types'][0]) {
+                                case 'locality':
+                                    $job->setCity($component['long_name']);
+                                    break;
+                                case 'postal_code':
+                                    $job->setZip($component['long_name']);
+                                    break;
+                                case 'administrative_area_level_2':
+                                    $department = $this->department->findOneByName(strtoupper($component['long_name']));
+                                    if ($department) {
+                                        $job->setDepartment($department);
+                                    }
+                                    break;
+                            }
+                        }
+                    }
+
+                    $this->em->persist($job);
+                    $this->em->flush();
+
+                }
+            }
+        }
+        die($job);
+    }
+
+    public function getFluxCSV($begin, $end) {
+
+        $companies = $this->company->findAll();
+
         foreach($companies as $company) {
             $flux = $company->getFluxXml();
 
@@ -97,7 +177,5 @@ class GetObjectFromFlux
                 }
             }
         }
-
-        die($job);
     }
 }
